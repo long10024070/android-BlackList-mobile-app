51ef7f232162b75a046de7a3e1460160fccba4a5
long10024070 <long10024070@gmail.com>
2022-05-17 20:24:23 +0700

    first commit

---
+++ app/blacklist/app/src/main/java/com/example/blacklist/MainActivity.java
@@ -0,0 +1,106 @@
+package com.example.blacklist;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.role.RoleManager;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.media.AudioAttributes;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.BlockedNumberContract;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.blacklist.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(binding.navView, navController);
+
+        // Request Default Call app
+        requestRole();
+
+        // Try block number
+        ContentValues values = new ContentValues();
+        values.put(BlockedNumberContract.BlockedNumbers.COLUMN_ORIGINAL_NUMBER, "1234567890");
+        Uri uri = getContentResolver().insert(BlockedNumberContract.BlockedNumbers.CONTENT_URI, values);
+
+        // Create call channel
+        NotificationChannel channel = new NotificationChannel("10203040503", "Incoming Calls",
+                NotificationManager.IMPORTANCE_HIGH);
+        Uri ringtoneUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE);
+        channel.setSound(ringtoneUri, new AudioAttributes.Builder()
+                // Setting the AudioAttributes is important as it identifies the purpose of your
+                // notification sound.
+                .setUsage(AudioAttributes.USAGE_NOTIFICATION_RINGTONE)
+                .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                .build());
+        NotificationManager mgr = getSystemService(NotificationManager.class);
+        mgr.createNotificationChannel(channel);
+    }
+
+    public void requestRole() {
+        RoleManager roleManager = (RoleManager) getSystemService(ROLE_SERVICE);
+        Intent intent = roleManager.createRequestRoleIntent(RoleManager.ROLE_DIALER);
+        startActivityResultLauncher.launch(intent);
+    }
+
+    ActivityResultLauncher<Intent> startActivityResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result != null && result.getResultCode() == android.app.Activity.RESULT_OK) {
+                        // Your app is now the default dialer app
+                    } else {
+                        // Your app is not the default dialer app
+                    }
+                }
+            });
+
+    public void blockNumber(View view) {
+        EditText phoneNumber = findViewById(R.id.BlockNumber);
+        ContentValues values = new ContentValues();
+        values.put(BlockedNumberContract.BlockedNumbers.COLUMN_ORIGINAL_NUMBER, phoneNumber.getText().toString());
+        Uri uri = getContentResolver().insert(BlockedNumberContract.BlockedNumbers.CONTENT_URI, values);
+    }
+
+    public void enterBlockNumber(View view) {
+        final String defaulContent = "Enter Number to Block";
+        EditText phoneNumber = findViewById(R.id.BlockNumber);
+        phoneNumber.getText().clear();
+    }
+}
